fc1f41ed2f31ea84290206493df1460b3ab785fd
[Blaine Prickett] <[blaineprickett@gmail.com]>
2023-07-20 21:58:47 -0500

    exercise complate

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can you guess my favorite number?");
+
+            int favoriteNumber = 7;
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+                string input = Console.ReadLine();
+                int userGuess = int.Parse(input);
+
+                if (userGuess > favoriteNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else if (userGuess < favoriteNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("You got it!");
+                    guessedCorrectly = true;
+                }
+            }
         }
     }
 }
